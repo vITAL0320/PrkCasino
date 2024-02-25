d4d078c89d1f9587f30db996597b5fc67a737ad1
vITAL320 <megashots26@gmail.com>
2024-02-25 12:15:32 +0500

    test

--- components/register.php
+++ components/register.php
@@ -12,17 +12,17 @@ if (empty($login) || empty($pass) || empty($repeatpass) || empty($email)) {
     $_SESSION['message_register'] = 'Заполните все поля';
     header('Location: ../#openModal-2');
 } else {
-    if ($pass != $repeatpass){
+    if ($pass != $repeatpass) {
         $_SESSION['message_register'] = 'Пароли не совпадают';
         header('Location: ../#openModal-2');
     } else {
         // Отправка в БД
         $sql = "INSERT INTO `users` (email, login, pass) VALUES ('$email', '$login', '$pass')";
         // Проверка запроса true или не true
-       if ($conn -> query($sql) === TRUE) {
-        echo 'Успешная регистрация';
-       } else {
-        echo "Ошибка: " .$conn->error;
-       }
+        if ($connect->query($sql) === TRUE) {
+            echo 'Успешная регистрация';
+        } else {
+            echo "Ошибка: " . $connect->error;
+        }
     }
 }
